5435e8876e5cab00729ae1ff4b8c7eb903e09266
ÇAĞRI TÜRKMEN <ARI TRKMEN@LAPTOP-DTGLMNBR>
2021-07-07 16:01:02 +0300

    Add project files.

---
+++ CreatingYourOwnDictionary/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreatingYourOwnDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] keys;
+        V[] values;
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K customerId, V customerName)
+        {
+            K[] tempKeys = keys;
+            V[] tempValues = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = customerId;
+            values[values.Length - 1] = customerName;
+
+
+        }
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+        public void ShowKeysValues()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Key: {0}        Value: {1} ", keys[i], values[i]);
+            }
+        }
+    }
+}
---
+++ CreatingYourOwnDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CreatingYourOwnDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------------------MY DICTIONARY--------------------------------------------------");
+
+            MyDictionary<int, string> myCustomerList = new MyDictionary<int, string>();
+
+            myCustomerList.Add(1, "Çağrı Türkmen");
+            myCustomerList.Add(2, "Derya Sarı");
+            myCustomerList.Add(3, "Hüseyin Kağan Arduç");
+
+            Console.WriteLine(myCustomerList.Count);
+
+            myCustomerList.ShowKeysValues();
+        }
+    }
+}
